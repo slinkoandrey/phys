d08b80057b8d93c041386615eee3f19111cda5cb
slinkoandrey <35749485+slinkoandrey@users.noreply.github.com>
2018-02-05 20:22:22 +0300

    Update quadr_eq.py

--- quadr_eq.py
+++ quadr_eq.py
@@ -2,16 +2,20 @@ import math
 import cmath
 
 def viet(b,c):
-    d = b**2 - 4*c
-    if d >= 0:
-        sqd = math.sqrt(d)
+    if (b == 0) and (c == 0):
+        x1 = 0
+        x2 = 0
     else:
-        sqd = cmath.sqrt(d)
-    if b>= 0:
-        x1 = (-b - sqd) / 2.0
-    else:
-        x1 = (-b + sqd) / 2.0
-    x2 = c/x1
+        d = b**2 - 4*c
+        if d >= 0:
+            sqd = math.sqrt(d)
+        else:
+            sqd = cmath.sqrt(d)
+        if b>= 0:
+            x1 = (-b - sqd) / 2.0
+        else:
+            x1 = (-b + sqd) / 2.0
+        x2 = c/x1
     return x1, x2
 
 def tailor(b,c):
